5a790169a4af3ddc64c052bf679d8c4a750c1ac0
jfrazier <johnfrazier1222@gmail.com>
2018-08-17 17:16:39 -0400

    new branch

--- db/migrate/20180817180102_create_users_table.rb
+++ db/migrate/20180817180102_create_users_table.rb
@@ -1,11 +1,12 @@
 class CreateUsersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :full_name
-      t.string :password
       t.string :email
-      t.string :created_at
-      t.string :upadated_at
+      t.string :name
+      t.string :password_hash
+      t.datetime :created_at
+      t.datetime :upadated_at
     end
+    add_index :users, :email, :unique: true
   end
 end
